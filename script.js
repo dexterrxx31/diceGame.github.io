93ce229ae61f6284b52466a61b306d1b4a6abf35
Riyan Ahmad <55991092+dexterrxx31@users.noreply.github.com>
2021-06-20 16:22:48 +0530

    Bug related to Dice Name Fixed

--- script.js
+++ script.js
@@ -3,23 +3,23 @@ var playerTwo = Math.floor(6 * Math.random() + 1);
 
 if (playerOne > playerTwo) {
   document.querySelector("h1").innerText = "Player One Won 🚩";
-} else if (playerTwo < playerTwo) {
+} else if (playerOne < playerTwo) {
   document.querySelector("h1").innerText = "Player Two Won 🚩";
 } else {
   document.querySelector("h1").innerText = "Match Draw 🚩";
 }
 if (playerOne === 1) {
-  document.querySelector("#dice2 img").setAttribute("src", "images/dice5.png");
+  document.querySelector("#dice1 img").setAttribute("src", "images/dice5.png");
 } else if (playerOne === 2) {
-  document.querySelector("#dice2 img").setAttribute("src", "images/dice2.png");
+  document.querySelector("#dice1 img").setAttribute("src", "images/dice2.png");
 } else if (playerOne === 3) {
-  document.querySelector("#dice2 img").setAttribute("src", "images/dice3.png");
+  document.querySelector("#dice1 img").setAttribute("src", "images/dice3.png");
 } else if (playerOne === 4) {
-  document.querySelector("#dice2 img").setAttribute("src", "images/dice4.png");
+  document.querySelector("#dice1 img").setAttribute("src", "images/dice4.png");
 } else if (playerOne === 5) {
-  document.querySelector("#dice2 img").setAttribute("src", "images/dice5.png");
+  document.querySelector("#dice1 img").setAttribute("src", "images/dice5.png");
 } else if (playerOne === 6) {
-  document.querySelector("#dice2 img").setAttribute("src", "images/dice6.png");
+  document.querySelector("#dice1 img").setAttribute("src", "images/dice6.png");
 }
 
 if (playerTwo === 1) {
