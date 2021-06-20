e80297298f4d9888f4d831e6331bee8b912d4386
Riyan Ahmad <riyanahmad99@gmail.com>
2021-06-20 15:10:54 +0530

    Final Commit

---
+++ script.js
@@ -0,0 +1,37 @@
+var playerOne = Math.floor(6 * Math.random() + 1);
+var playerTwo = Math.floor(6 * Math.random() + 1);
+
+if (playerOne > playerTwo) {
+  document.querySelector("h1").innerText = "Player One Won 🚩";
+} else if (playerTwo < playerTwo) {
+  document.querySelector("h1").innerText = "Player Two Won 🚩";
+} else {
+  document.querySelector("h1").innerText = "Match Draw 🚩";
+}
+if (playerOne === 1) {
+  document.querySelector("#dice2 img").setAttribute("src", "images/dice5.png");
+} else if (playerOne === 2) {
+  document.querySelector("#dice2 img").setAttribute("src", "images/dice2.png");
+} else if (playerOne === 3) {
+  document.querySelector("#dice2 img").setAttribute("src", "images/dice3.png");
+} else if (playerOne === 4) {
+  document.querySelector("#dice2 img").setAttribute("src", "images/dice4.png");
+} else if (playerOne === 5) {
+  document.querySelector("#dice2 img").setAttribute("src", "images/dice5.png");
+} else if (playerOne === 6) {
+  document.querySelector("#dice2 img").setAttribute("src", "images/dice6.png");
+}
+
+if (playerTwo === 1) {
+  document.querySelector("#dice2 img").setAttribute("src", "images/dice5.png");
+} else if (playerTwo === 2) {
+  document.querySelector("#dice2 img").setAttribute("src", "images/dice2.png");
+} else if (playerTwo === 3) {
+  document.querySelector("#dice2 img").setAttribute("src", "images/dice3.png");
+} else if (playerTwo === 4) {
+  document.querySelector("#dice2 img").setAttribute("src", "images/dice4.png");
+} else if (playerTwo === 5) {
+  document.querySelector("#dice2 img").setAttribute("src", "images/dice5.png");
+} else if (playerTwo === 6) {
+  document.querySelector("#dice2 img").setAttribute("src", "images/dice6.png");
+}
